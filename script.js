142ccfde0afa0fa9fa2e577d389f8045d2a2853e
Ariel Jr Mimura <avmimurajr@gmail.com>
2024-04-02 15:57:28 +0800

    completed downloadPDF function

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-// function downloadPDF() {
-//     html2canvas(document.querySelector("#download-body"), {
-//         windowWidth: 4096,
-//         windowHeight: 2200
-//     }).then(canvas => {
-//         // Initialize jsPDF
-//         var pdf = new jspdf.jsPDF('p', 'pt', [canvas.width, canvas.height]);
-        
-//         // Add image to PDF
-//         var imgData = canvas.toDataURL('image/jpeg', 1.0);
-//         pdf.addImage(imgData, 'JPEG', 0, 0, canvas.width, canvas.height);
-        
-//         // Save the PDF
-//         pdf.save("MIMURA_ARIEL_CV.pdf");
-//     });
-// }
-
 function downloadPDF() {
+
+    prepareHtmlPage();
+
     html2canvas(document.body, {
         windowWidth: 2560,
         windowHeight: 1500
@@ -31,3 +17,24 @@ function downloadPDF() {
         pdf.save("MIMURA_ARIEL_CV.pdf");
     });
 }
+
+function prepareHtmlPage(){
+
+    // Remove all bullet points
+    const uls = document.querySelectorAll('ul');
+    uls.forEach(ul => {
+        ul.style.listStyleType = 'none';
+    });
+
+    // Insert margin top to fix pdf paging when downloaded
+    const fName = document.getElementsByClassName('name')[0];
+    fName.style.marginTop = '2em';
+
+    // Hide all buttons
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => {
+        button.style.display = 'none';
+    });
+}
+
+// TODO: Create function to revert changes done in prepareHtmlPage()
